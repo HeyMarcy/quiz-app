045763bbfa54b2b32be791c42aa2301b28febdd0
Terry Hayes <terrychayes@gmail.com>
2017-02-06 15:10:27 -0500

    Adds displayQuestion function. Initial splash page and first questions displaying properly.

--- app.js
+++ app.js
@@ -17,19 +17,27 @@ var state = {
         answers: ["Job loss", "Medical bills", "Divorce", "Failure to stick to a budget"],
         correct: 1}],
     score: 0,
-    qIndicator = 0,
-    
+    qIndicator : 1,
+
 }
 
+function displayQuestion(state){
+  var responses = []
+  state.questions[state.qIndicator].answers.forEach(function (answer){
+    responses.push('<li> <button>' + answer + '</button></li>')
+  });
+  var quizTemplate = '<p>' + 'Question ' + (state.qIndicator+1) + '</p>' +
+                      '<h4>'+ state.questions[state.qIndicator].qText + '</h4>' +
+                      '<ul>' + responses + '</ul>' +
+                      '<button> NEXT </button>';
+  $('#main').html(quizTemplate);
+};
 
-var splashTemplate = '<h1>Financial Literacy Quiz</h1>' + 
-    '<h4> Terry and marcy have teamed up to test your financial literacy!</h4>'  +
-    '<button>Let\'s go!</button>'
+function eventHandlers(){
+  $('#js-begin').click(function(event){
+      displayQuestion(state);
+  });
+};
 
 
-var responses = '<li> <button>' + state.questions[i].answers[j] + '</button></li>';
-
-var quizTemplate = '<p>' +state.qIndicator+1 + '</p>' + 
-                    '<h4>'+ state.questions[i].qText + '</h4>' +
-                    '<ul>' + questionlist + '</ul>' + 
-                    '<button> NEXT </button>';
+$(eventHandlers);
