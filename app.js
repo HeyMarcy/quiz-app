0fa1ada2d6e076260c92e87d1bb5ddea06faf53c
Terry Hayes <terrychayes@gmail.com>
2017-02-06 12:15:59 -0500

    Initial commit.

---
+++ app.js
@@ -0,0 +1,9 @@
+
+var state = {
+    questions: ["1. Which is considered an average credit score on the FICO scale?", "In order to avoid a penalty tax, distributions from an IRA must begin the year in which you attain the age of:"
+"],
+    choices: [["400", "630", "720", "1440"], ["55", "59 1/2", "65"], [], [], []]
+    correctChoice: 2
+    ],
+    score:0
+}
