3164ecf345b3e9b6e50059b38274ceda8f3bbf9c
Terry Hayes <terrychayes@gmail.com>
2017-02-06 12:28:14 -0500

    Adds questions and answers to app.js state object

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 
 var state = {
-    questions: ["1. Which is considered an average credit score on the FICO scale?", "In order to avoid a penalty tax, distributions from an IRA must begin the year in which you attain the age of:"
-"],
-    choices: [["400", "630", "720", "1440"], ["55", "59 1/2", "65"], [], [], []]
-    correctChoice: 2
-    ],
-    score:0
+    questions: ["1. Which is considered an average credit score on the FICO scale?",
+    "2. In order to avoid a penalty tax, distributions from an IRA must begin the year in which you attain the age of:",
+    "3. If you check your credit reports once a year, how much will that typically cost?",
+    "4. When do you pay taxes on the money you put into a traditional 401(k)?",
+    "5. What is the biggest cause of personal bankruptcy in the United States?"
+  ],
+    answers: [["400", "630", "720", "1440"], ["55", "59 1/2", "65", "70 1/2"], ["$90", "$0", "$30", "$60"],
+    ["When you withdraw the money", "Never, if you wait until retirement", "When you earn the money", "Every year, by April 15"],
+    ["Job loss", "Medical bills", "Divorce", "Failure to stick to a budget"]]
+    correctAnswers: [2,4,2,1,2], //Note that these are not by index at this point
+    score: 0
 }
