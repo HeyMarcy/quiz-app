e37162f851b0c21b7b5bcfa79a1874ccad74c61f
Terry Hayes <terrychayes@gmail.com>
2017-02-07 11:48:44 -0500

    Adds basic CSS outline; fixes issue with explanation text in state object.

--- app.js
+++ app.js
@@ -7,19 +7,19 @@ var state = {
       explanation: "A FICO credit score of 630 to 689 is average; a score of 720 or higher is considered excellent. Most lenders use the FICO scale when deciding whether to give you a loan or a credit card and what interest rate you’ll pay if you are approved. FICO credit scores, developed by Fair Isaac Corp., range from 300 to 850."},
       {qText: "In order to avoid a penalty tax, distributions from an IRA must begin the year in which you attain the age of:",
       answers: ["55", "59 1/2", "65", "70 1/2"],
-      correct: 3
+      correct: 3,
       explanation: "Most respondents knew that the IRS requires taxpayers to withdraw a minimum amount, known as a required minimum distribution, from their retirement accounts annually starting the year in which they turn 70½."},
       {qText: "If you check your credit reports once a year, how much will that typically cost?",
       answers: ["$90", "$0", "$30", "$60"],
-      correct: 1
+      correct: 1,
       explanation: "Federal law guarantees you access to a free credit report once a year from each of the three major credit rating companies, Experian, TransUnion and Equifax."},
       {qText: "When do you pay taxes on the money you put into a traditional 401(k)?",
       answers: ["When you withdraw the money", "Never, if you wait until retirement", "When you earn the money", "Every year, by April 15"],
-      correct: 0
+      correct: 0,
       explanation: "Taxes are deferred on earnings diverted to a 401(k) savings plan. You’ll pay regular income taxes at the time you withdraw the money, and you’ll pay a 10% penalty on top of your taxes for any withdrawals made before age 59½."},
       {qText: "What is the biggest cause of personal bankruptcy in the United States?",
       answers: ["Job loss", "Medical bills", "Divorce", "Failure to stick to a budget"],
-      correct: 1
+      correct: 1,
       explanation: "Studies by NerdWallet Health and Harvard University have found that a majority of all personal bankruptcies in the U.S. are caused by medical expenses. Filers in most of those bankruptcy cases can’t pay their medical debts despite having health insurance."}],
     score: 0,
     qIndicator : 0,
